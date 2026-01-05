58af9de14a328898d475ad274ccc4c5413240380
ANAMIKA VISH <av.anamika1@gmail.com>
2026-01-05 11:00:04 +0530

    fixed the chnages

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -14,70 +14,74 @@ class BillsController < ApplicationController
       items_params = items_params.values
     end
 
-    ActiveRecord::Base.transaction do
-      items_params.each do |item_data|
-        prod_id = item_data["product_id"]
-        qty = item_data["quantity"].to_i
+    begin
+      ActiveRecord::Base.transaction do
+        items_params.each do |item_data|
+          prod_id = item_data["product_id"]
+          qty = item_data["quantity"].to_i
 
-        next if prod_id.blank? || qty <= 0
+          next if prod_id.blank? || qty <= 0
 
-        product = Product.find_by(product_id: prod_id)
-        next unless product
+          product = Product.find_by(product_id: prod_id)
+          next unless product
 
-        unit_price = product.price
-        tax_pct = product.tax_percentage
+          unit_price = product.price
+          tax_pct = product.tax_percentage
 
-        purchase_price = unit_price * qty
-        tax_payable = purchase_price * (tax_pct / 100)
-        total_price = purchase_price + tax_payable
+          purchase_price = unit_price * qty
+          tax_payable = purchase_price * (tax_pct / 100.0)
+          total_price = purchase_price + tax_payable
 
-        @purchase.purchase_items.build(
-          product: product,
-          quantity: qty,
-          unit_price: unit_price,
-          tax_percentage: tax_pct,
-          purchase_price: purchase_price,
-          tax_payable: tax_payable,
-          total_price: total_price
-        )
+          @purchase.purchase_items.build(
+            product: product,
+            quantity: qty,
+            unit_price: unit_price,
+            tax_percentage: tax_pct,
+            purchase_price: purchase_price,
+            tax_payable: tax_payable,
+            total_price: total_price
+          )
 
-        total_without_tax += purchase_price
-        total_tax += tax_payable
+          total_without_tax += purchase_price
+          total_tax += tax_payable
 
-        # Update stock
-        product.update!(stock: product.stock - qty)
-      end
+          # Update stock (using update instead of update! to avoid stopping on validation if possible,
+          # though model validation might still trigger if stock < 0)
+          product.update(stock: product.stock - qty)
+        end
 
-      @purchase.total_without_tax = total_without_tax
-      @purchase.total_tax = total_tax
-      @purchase.net_price = total_without_tax + total_tax
-      @purchase.rounded_net_price = @purchase.net_price.to_i
-      @purchase.balance_payable = @purchase.paid_amount - @purchase.rounded_net_price
+        @purchase.total_without_tax = total_without_tax
+        @purchase.total_tax = total_tax
+        @purchase.net_price = total_without_tax + total_tax
+        @purchase.rounded_net_price = @purchase.net_price.to_i
+        @purchase.balance_payable = @purchase.paid_amount - @purchase.rounded_net_price
 
-      # Process denominations into a hash: { "500" => 2, "50" => 1, ... }
-      denoms_data = params[:denominations] || {}
-      collected = {}
-      if denoms_data.respond_to?(:values)
-        denoms_data.values.each do |d|
-          val = d["value"].to_s
-          count = d["count"].to_i
-          collected[val] = count if count > 0
+        # Process denominations into a hash: { "500" => 2, "50" => 1, ... }
+        denoms_data = params[:denominations] || {}
+        collected = {}
+        if denoms_data.respond_to?(:values)
+          denoms_data.values.each do |d|
+            val = d["value"].to_s
+            count = d["count"].to_i
+            collected[val] = count if count > 0
+          end
+        end
+        @purchase.denominations_collected = collected
+
+        if @purchase.save
+          # Send invoice asynchronously
+          InvoiceMailer.send_invoice(@purchase).deliver_later
+          redirect_to bill_path(@purchase)
+        else
+          render :new
         end
       end
-      @purchase.denominations_collected = collected
-
-      if @purchase.save
-        # Send invoice asynchronously
-        InvoiceMailer.send_invoice(@purchase).deliver_later
-        redirect_to bill_path(@purchase)
-      else
-        render :new
-      end
+    rescue StandardError => e
+      flash.now[:error] = e.message
+      render :new
     end
-  rescue ActiveRecord::RecordInvalid => e
-    flash[:error] = e.message
-    render :new
-end
+  end
+
 
   def show
     @purchase = Purchase.find(params[:id])
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
   validates :name, :product_id, :price, presence: true
   validates :product_id, uniqueness: true
-  validates :stock, numericality: { greater_than_or_equal_to: 0 }
 end
